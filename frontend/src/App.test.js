ebb087740a09aa997d585f62f1a1c44b461a2849
Jingjing Yang <jingjing.yang@tuni.fi>
2022-12-04 03:38:28 +0200

    Update App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from '../todoApp/App';
+import App from './todoApp/App';
 
 test('renders learn react link', () => {
   render(<App />);
